7d086409516bc342e27f29f80362c16fd44b2f9f
xmaniaxz <langewesley11@gmail.com>
2022-02-17 21:09:08 +0100

    6th commit

---
+++ Assets/DumpFiles/Dump Nathan/Investing in the future/TimeLine/Scripts/BaseDynamicHandler.cs
@@ -0,0 +1,10 @@
+using SimpleWebBrowser;
+using UnityEngine;
+
+public class BaseDynamicHandler : MonoBehaviour, IDynamicRequestHandler
+{
+    public virtual string Request(string url, string query)
+    {
+        return null;
+    }
+}
